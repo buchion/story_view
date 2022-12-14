5121e4ca9c4d5a3bcd674592163c5ec90e95a1d6
Buchi <buchionwuasoanya@hotmail.com>
2022-12-14 23:26:31 +0100

    Added hervest design

--- lib/widgets/story_view.dart
+++ lib/widgets/story_view.dart
@@ -51,6 +51,97 @@ class StoryItem {
   /// Works for inline and full-page stories. See [StoryView.inline] for more on
   /// what inline/full-page means.
   ///
+  
+    static StoryItem hervestTextStory({
+    required String title,
+    required String subtitle,
+    // required Widget image,
+    String? chipHeader,
+    TextStyle? chipHeaderStyle,
+    Widget? storyHeaderIcon,
+    Widget? chipAvatar,
+    StadiumBorder? borderside,
+    required Color backgroundColor,
+    Key? key,
+    TextStyle? textStyle,
+    bool shown = false,
+    bool roundedTop = false,
+    bool roundedBottom = false,
+    Duration? duration,
+  }) {
+    double contrast = ContrastHelper.contrast([
+      backgroundColor.red,
+      backgroundColor.green,
+      backgroundColor.blue,
+    ], [
+      255,
+      255,
+      255
+    ] /** white text */);
+
+    return StoryItem(
+      Container(
+        key: key,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(roundedTop ? 8 : 0),
+            bottom: Radius.circular(roundedBottom ? 8 : 0),
+          ),
+        ),
+        padding: EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 50),
+              Chip(
+                avatar: chipAvatar,
+                shape: borderside,
+                backgroundColor: Color(0xFF5B2E4F),
+                label: Text(
+                  '',
+                  style: chipHeaderStyle,
+                ),
+              ),
+              SizedBox(height: 50),
+              Text(
+                title,
+                style: textStyle?.copyWith(
+                      color: contrast > 1.8 ? Colors.white : Colors.black,
+                    ) ??
+                    TextStyle(
+                      color: contrast > 1.8 ? Colors.white : Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                textAlign: TextAlign.left,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                subtitle,
+                style: textStyle?.copyWith(
+                      color: contrast > 1.8 ? Colors.white : Colors.black,
+                    ) ??
+                    TextStyle(
+                      color: contrast > 1.8 ? Colors.white : Colors.black,
+                      fontSize: 24,
+                      fontWeight: FontWeight.w700,
+                    ),
+                textAlign: TextAlign.left,
+              ),
+              
+            ],
+          ),
+      ),
+      shown: shown,
+      duration: duration ?? Duration(seconds: 12),
+    );
+  }
 
   static StoryItem hervestCustomStory({
     required String title,
